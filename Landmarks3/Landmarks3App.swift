0aa90645a9f98a5399aee90f45518f9c8c9277f9
sawa <you@example.com>
2024-02-15 12:52:43 +0900

    Initial Commit

---
+++ Landmarks3/Landmarks3App.swift
@@ -0,0 +1,17 @@
+//
+//  Landmarks3App.swift
+//  Landmarks3
+//
+//  Created by Apple on 2024/02/15.
+//
+
+import SwiftUI
+
+@main
+struct Landmarks3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
